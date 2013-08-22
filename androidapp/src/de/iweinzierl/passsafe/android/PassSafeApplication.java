3508d2ad2f289b260f307d1243bccf20402fe4d5
Ingo Weinzierl <weinzierl.ingo@gmail.com>
2013-08-22 20:01:40 +0200

    Fixed broken version after Gradle -> Maven switch.

--- androidapp/src/de/iweinzierl/passsafe/android/PassSafeApplication.java
+++ androidapp/src/de/iweinzierl/passsafe/android/PassSafeApplication.java
@@ -20,7 +20,6 @@ public class PassSafeApplication extends Application {
 
     private void initializeSingletons() {
         LOGGER.info("initializeSingletons()");
-        LOGGER.debug(new Test("This is my personal test!").toString());
     }
 
     public void setPassword(String password) {
