02456a7fab76ed0280f35d5a0d23960214cd230a
Ingo Weinzierl <ingo.weinzierl@zalando.de>
2014-02-17 06:44:53 +0100

    Refactored the startup dialog to use the new PasswortInputPanel.

--- java-gui/src/main/java/de/iweinzierl/passsafe/gui/widget/StartupDialogBuilder.java
+++ java-gui/src/main/java/de/iweinzierl/passsafe/gui/widget/StartupDialogBuilder.java
@@ -1,16 +1,11 @@
 package de.iweinzierl.passsafe.gui.widget;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 import de.iweinzierl.passsafe.gui.resources.Messages;
 import de.iweinzierl.passsafe.gui.util.UiUtils;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPasswordField;
-import javax.swing.JRootPane;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
 
 public class StartupDialogBuilder {
 
@@ -22,77 +17,47 @@ public class StartupDialogBuilder {
 
     public static final int DIALOG_WIDTH = 315;
     public static final int DIALOG_HEIGHT = 150;
-    public static final int DIALOG_LABEL_WIDTH = 325;
-    public static final int DIALOG_LABEL_HEIGHT = 25;
-    public static final int BUTTON_WIDTH = 125;
-    public static final int BUTTON_HEIGHT = 25;
-    public static final int PASSWORD_FIELD_WIDTH = 175;
-    public static final int PASSWORD_FIELD_HEIGHT = 25;
-    public static final int PASSWORD_LABEL_WIDTH = 75;
-    public static final int PASSWORD_LABEL_HEIGHT = 25;
 
     private ActionListener actionListener;
 
-    private JPasswordField passwordField;
-    private JButton okButton;
-    private JButton cancelButton;
+    public StartupDialogBuilder() { }
 
-    public StartupDialogBuilder() {
-    }
-
-    public StartupDialogBuilder setActionListener(ActionListener actionListener) {
+    public StartupDialogBuilder setActionListener(final ActionListener actionListener) {
         this.actionListener = actionListener;
         return this;
     }
 
     public void build() {
         final JFrame dialog = new JFrame();
-        passwordField = (JPasswordField) WidgetFactory.createComponent(JPasswordField.class, PASSWORD_FIELD_WIDTH,
-                PASSWORD_FIELD_HEIGHT);
 
-        okButton = WidgetFactory.createButton(Messages.getMessage(Messages.START_DIALOG_OK_BUTTON), BUTTON_WIDTH,
-                BUTTON_HEIGHT);
+        //J-
+        EnterPasswordPanel enterPasswordPanel = new EnterPasswordPanel.Builder()
+                .withTitle(Messages.getMessage(Messages.CHANGEPASSWORD_PANEL_TITLE))
+                .withLabel(Messages.getMessage(Messages.CHANGEPASSWORD_PANEL_LABEL))
+                .withListener(new EnterPasswordPanel.ActionListener() {
+                    @Override
+                    public void submitted(final String password) {
+                        dialog.dispose();
+                        actionListener.submitted(password);
+                    }
 
-        cancelButton = WidgetFactory.createButton(Messages.getMessage(Messages.START_DIALOG_CANCEL_BUTTON),
-                BUTTON_WIDTH, BUTTON_HEIGHT);
-
-        okButton.addActionListener(new java.awt.event.ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                actionListener.submitted(passwordField.getText());
-                dialog.dispose();
-            }
-        });
-
-        cancelButton.addActionListener(new java.awt.event.ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                actionListener.canceled();
-                dialog.dispose();
-            }
-        });
+                    @Override
+                    public void canceled() {
+                        dialog.dispose();
+                        actionListener.canceled();
+                    }
+                }).build();
+        //J+
 
         dialog.setTitle(Messages.getMessage(Messages.START_DIALOG_TITLE));
-        JRootPane rootPane = dialog.getRootPane();
-        rootPane.setLayout(new FlowLayout(FlowLayout.LEFT));
-
-        rootPane.add(WidgetFactory.createLabel(Messages.getMessage(Messages.START_DIALOG_LABEL), DIALOG_LABEL_WIDTH,
-                DIALOG_LABEL_HEIGHT));
-
-        rootPane.add(WidgetFactory.createInputPanel(
-                WidgetFactory.createLabel(Messages.getMessage(Messages.START_DIALOG_PASSWORD_LABEL),
-                        PASSWORD_LABEL_WIDTH, PASSWORD_LABEL_HEIGHT), passwordField));
-
-        rootPane.add(WidgetFactory.createButtonPanel(okButton, cancelButton));
+        dialog.setContentPane(enterPasswordPanel);
 
         dialog.setPreferredSize(new Dimension(DIALOG_WIDTH, DIALOG_HEIGHT));
         dialog.setMinimumSize(new Dimension(DIALOG_WIDTH, DIALOG_HEIGHT));
         dialog.setResizable(false);
         dialog.pack();
-        dialog.show();
+        dialog.setVisible(true);
 
         UiUtils.center(dialog);
     }
 }
-
