83280636be382a5a84d2e12bc0cb5dcf5e57b722
Ingo Weinzierl <ingo.weinzierl@zalando.de>
2014-02-26 21:00:45 +0100

    Added last_modified columns to all tables in database schema.

--- shared/src/main/java/de/iweinzierl/passsafe/shared/data/SQLiteDatabaseCreator.java
+++ shared/src/main/java/de/iweinzierl/passsafe/shared/data/SQLiteDatabaseCreator.java
@@ -14,12 +14,16 @@ public class SQLiteDatabaseCreator {
     }
 
     public static final String SQL_CREATE_CATEGORY = "CREATE TABLE category ("
-            + "  _id INTEGER PRIMARY KEY AUTOINCREMENT, " + "  title TEXT NOT NULL" + ");";
+            + "  _id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, last_modified TEXT NOT NULL);";
 
-    public static final String SQL_CREATE_ENTRY = "CREATE TABLE entry ( " + "  _id INTEGER PRIMARY KEY AUTOINCREMENT, "
+    public static final String SQL_CREATE_ENTRY = "CREATE TABLE entry (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
             + "  category_id INTEGER NOT NULL, " + "  title TEXT NOT NULL, " + "  url TEXT, " + "  username TEXT, "
-            + "  password TEXT NOT NULL, " + "  comment  TEXT, "
-            + "  FOREIGN KEY (category_id) REFERENCES category(_id)" + ");";
+            + "  password TEXT NOT NULL, " + "  comment  TEXT, last_modified TEXT NOT NULL, "
+            + "  FOREIGN KEY (category_id) REFERENCES category(_id));";
+
+    public static final String SQL_CREATE_ANDROID_METADATA = "CREATE TABLE android_metadata (locale text);";
+
+    public static final String SQL_INSERT_ANDROID_METADATA = "INSERT INTO android_metadata (locale) VALUES ('en_US');";
 
     private final SQLiteCommandExecutor commandExecutor;
     private final OS os;
@@ -32,5 +36,10 @@ public class SQLiteDatabaseCreator {
     public void setup() throws PassSafeSqlException {
         commandExecutor.execute(SQL_CREATE_CATEGORY);
         commandExecutor.execute(SQL_CREATE_ENTRY);
+
+        if (os != OS.ANDROID) {
+            commandExecutor.execute(SQL_CREATE_ANDROID_METADATA);
+            commandExecutor.execute(SQL_INSERT_ANDROID_METADATA);
+        }
     }
 }
