953e12c00fb9d2226f4b3beeb9db6ee07216060b
Ingo Weinzierl <ingo.weinzierl@zalando.de>
2014-02-27 19:27:52 +0100

    Encapsulated download of files from GoogleDrive in Android app from general sync mechanism.

---
+++ androidapp/src/de/iweinzierl/passsafe/android/sync/gdrive/GoogleDriveDownload.java
@@ -0,0 +1,148 @@
+package de.iweinzierl.passsafe.android.sync.gdrive;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import java.util.Date;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.DriveFile;
+import com.google.android.gms.drive.Metadata;
+import com.google.android.gms.drive.MetadataBuffer;
+import com.google.android.gms.drive.metadata.StringMetadataField;
+import com.google.android.gms.drive.query.Filters;
+import com.google.android.gms.drive.query.Query;
+
+import android.content.Context;
+
+import de.iweinzierl.passsafe.android.logging.Logger;
+import de.iweinzierl.passsafe.android.util.FileUtils;
+
+public class GoogleDriveDownload implements ResultCallback<DriveApi.MetadataBufferResult> {
+
+    private static final String PASSSAFE_DATABASE_FILE = "passsafe.sqlite";
+
+    private static final Logger LOGGER = new Logger("GoogleDriveDownload");
+
+    private final Context context;
+    private final GoogleDriveSync googleDriveSync;
+    private final GoogleApiClient googleApiClient;
+
+    public GoogleDriveDownload(final Context context, final GoogleDriveSync googleDriveSync,
+            final GoogleApiClient googleApiClient) {
+        this.context = context;
+        this.googleDriveSync = googleDriveSync;
+        this.googleApiClient = googleApiClient;
+    }
+
+    /**
+     * Download PassSafe database file from GoogleDrive online account. If download is finished
+     * {@link de.iweinzierl.passsafe.android.sync.gdrive.GoogleDriveSync#onUpdate(de.iweinzierl.passsafe.android.sync.gdrive.GoogleDriveSync.State)}
+     * is called.
+     */
+    public void download() {
+        //J-
+        PendingResult<DriveApi.MetadataBufferResult> queryResult = Drive.DriveApi.query(googleApiClient,
+                new Query.Builder()
+                        .addFilter(
+                                Filters.and(
+                                        Filters.eq(new StringMetadataField("title"), PASSSAFE_DATABASE_FILE),
+                                        Filters.eq(new StringMetadataField("trashed"), "false")))
+                        .build());
+        //J+
+
+        queryResult.setResultCallback(this);
+    }
+
+    /**
+     * Called after GoogleDrive search for PassSafe database file is finished.
+     *
+     * @param  result  The search result.
+     */
+    @Override
+    public void onResult(final DriveApi.MetadataBufferResult result) {
+        if (!result.getStatus().isSuccess()) {
+            LOGGER.warn(String.format("Did not find a '%s' file in GoogleDrive account", PASSSAFE_DATABASE_FILE));
+
+        } else {
+            MetadataBuffer metadataBuffer = result.getMetadataBuffer();
+
+            if (metadataBuffer.getCount() > 0) {
+                LOGGER.debug("Found " + metadataBuffer.getCount() + " results for search");
+                download(metadataBuffer.get(0));
+            } else {
+                LOGGER.warn("No files found for synchronization");
+                googleDriveSync.onUpdate(GoogleDriveSync.State.DOWNLOAD_CANCELED);
+            }
+        }
+    }
+
+    private void download(final Metadata metadata) {
+        File dbFile = FileUtils.getDatabaseFile(context);
+        LOGGER.debug(String.format("Local database file is located at: %s", dbFile.getAbsolutePath()));
+        LOGGER.debug(String.format("Local database file is %s big", dbFile.length()));
+
+        if (isDownloadRequired(dbFile, metadata)) {
+            LOGGER.debug("Download of database required");
+            download(dbFile, metadata);
+        } else {
+            LOGGER.debug("Download is not required");
+            googleDriveSync.onUpdate(GoogleDriveSync.State.DOWNLOAD_FINISHED);
+        }
+    }
+
+    private boolean isDownloadRequired(final File dbFile, final Metadata metadata) {
+        if (!dbFile.exists()) {
+            return true;
+        }
+
+        long dbFileLastModified = dbFile.lastModified();
+        long onlineLastModified = metadata.getModifiedDate().getTime();
+
+        LOGGER.debug("Current local time:       " + new Date(System.currentTimeMillis()));
+        LOGGER.debug("Last local  modification: " + new Date(dbFileLastModified));
+        LOGGER.debug("Last online modification: " + new Date(onlineLastModified));
+
+        return onlineLastModified > dbFileLastModified;
+    }
+
+    private void download(final File dbFile, final Metadata metadata) {
+        LOGGER.debug("Download online database file now...");
+
+        DriveFile file = Drive.DriveApi.getFile(googleApiClient, metadata.getDriveId());
+        PendingResult<DriveApi.ContentsResult> contentsResultPendingResult = file.openContents(googleApiClient,
+                DriveFile.MODE_READ_ONLY, new DriveFile.DownloadProgressListener() {
+                    @Override
+                    public void onProgress(final long downloaded, final long expected) {
+                        LOGGER.debug(String.format("Downloaded %s of %s", downloaded, expected));
+                    }
+                }); // add listener here
+
+        contentsResultPendingResult.setResultCallback(new ResultCallback<DriveApi.ContentsResult>() {
+                @Override
+                public void onResult(final DriveApi.ContentsResult contentsResult) {
+                    LOGGER.debug("Copy online database file to disk");
+                    try {
+                        InputStream in = contentsResult.getContents().getInputStream();
+                        OutputStream out = new FileOutputStream(dbFile);
+
+                        org.apache.commons.io.IOUtils.copy(in, out);
+
+                        LOGGER.info(String.format("Download finished: %s bytes", dbFile.length()));
+
+                        googleDriveSync.onUpdate(GoogleDriveSync.State.DOWNLOAD_FINISHED);
+
+                    } catch (IOException e) {
+                        LOGGER.error("Unable to download database file", e);
+                    }
+                }
+            });
+    }
+}
--- androidapp/src/de/iweinzierl/passsafe/android/sync/gdrive/GoogleDriveSync.java
+++ androidapp/src/de/iweinzierl/passsafe/android/sync/gdrive/GoogleDriveSync.java
@@ -1,25 +1,8 @@
 package de.iweinzierl.passsafe.android.sync.gdrive;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-import java.util.Date;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.drive.Drive;
-import com.google.android.gms.drive.DriveApi;
-import com.google.android.gms.drive.DriveFile;
-import com.google.android.gms.drive.Metadata;
-import com.google.android.gms.drive.MetadataBuffer;
-import com.google.android.gms.drive.metadata.StringMetadataField;
-import com.google.android.gms.drive.query.Filters;
-import com.google.android.gms.drive.query.Query;
 
 import android.app.Activity;
 
@@ -28,26 +11,35 @@ import android.content.IntentSender;
 import android.os.Bundle;
 
 import de.iweinzierl.passsafe.android.logging.Logger;
-import de.iweinzierl.passsafe.android.util.FileUtils;
 
-public class GoogleDriveSync implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
-    ResultCallback<DriveApi.MetadataBufferResult> {
+public class GoogleDriveSync implements GoogleApiClient.ConnectionCallbacks,
+    GoogleApiClient.OnConnectionFailedListener {
 
     public interface Callback {
         void onSyncFinished();
     }
 
+    public enum State {
+        DOWNLOAD_REQUESTED,
+        DOWNLOAD_CANCELED,
+        DOWNLOAD_FINISHED,
+        UPLOAD_REQUESTED,
+        DATABASE_SYNC_REQUESTED,
+        COMPLETED
+    }
+
     private static final Logger LOGGER = new Logger("GoogleDriveSync");
-    private static final String PASSSAFE_DATABASE_FILE = "passsafe.sqlite";
 
     private final Activity activity;
     private final GoogleApiClient googleApiClient;
 
     private Callback callback;
+    private State currentState;
     private boolean syncRequested;
 
     public GoogleDriveSync(final Activity activity) {
         this.activity = activity;
+        this.currentState = State.DOWNLOAD_REQUESTED;
 
         //J-
         googleApiClient = new GoogleApiClient.Builder(activity)
@@ -68,18 +60,46 @@ public class GoogleDriveSync implements GoogleApiClient.ConnectionCallbacks, Goo
             syncRequested = true;
             connect();
         } else {
+            onUpdate(currentState);
+        }
+    }
 
-            //J-
-            PendingResult<DriveApi.MetadataBufferResult> queryResult = Drive.DriveApi.query(googleApiClient,
-                new Query.Builder()
-                        .addFilter(
-                                Filters.and(
-                                        Filters.eq(new StringMetadataField("title"), PASSSAFE_DATABASE_FILE),
-                                        Filters.eq(new StringMetadataField("trashed"), "false")))
-                        .build());
-            //J+
+    public void onUpdate(final State newState) {
+        currentState = newState;
 
-            queryResult.setResultCallback(this);
+        switch (currentState) {
+
+            case DOWNLOAD_REQUESTED :
+
+                new GoogleDriveDownload(activity, this, googleApiClient).download();
+                break;
+
+            case DOWNLOAD_CANCELED :
+
+                callback.onSyncFinished();
+                break;
+
+            case DOWNLOAD_FINISHED :
+
+                onUpdate(State.DATABASE_SYNC_REQUESTED);
+                break;
+
+            case UPLOAD_REQUESTED :
+
+                // TODO
+                callback.onSyncFinished();
+                break;
+
+            case DATABASE_SYNC_REQUESTED :
+
+                // TODO
+                callback.onSyncFinished();
+                break;
+
+            case COMPLETED :
+
+                callback.onSyncFinished();
+                break;
         }
     }
 
@@ -118,97 +138,5 @@ public class GoogleDriveSync implements GoogleApiClient.ConnectionCallbacks, Goo
                 connect();
             }
         }
-    }
-
-    @Override
-    public void onResult(final DriveApi.MetadataBufferResult result) {
-        if (!result.getStatus().isSuccess()) {
-            LOGGER.warn(String.format("Did not find a '%s' file in GoogleDrive account", PASSSAFE_DATABASE_FILE));
-
-        } else {
-            MetadataBuffer metadataBuffer = result.getMetadataBuffer();
-
-            if (metadataBuffer.getCount() > 0) {
-                LOGGER.debug("Found " + metadataBuffer.getCount() + " results for search");
-                sync(metadataBuffer.get(0));
-            } else {
-                LOGGER.warn("No files found for synchronization");
-                callback.onSyncFinished();
-            }
-        }
-    }
-
-    private void sync(final Metadata metadata) {
-        File dbFile = FileUtils.getDatabaseFile(activity);
-        LOGGER.debug(String.format("Local database file is located at: %s", dbFile.getAbsolutePath()));
-        LOGGER.debug(String.format("Local database file is %s big", dbFile.length()));
-
-        if (isDownloadRequired(dbFile, metadata)) {
-            LOGGER.debug("Download of database required");
-            download(dbFile, metadata);
-        } else if (isUploadRequired(dbFile, metadata)) {
-            LOGGER.debug("Upload of database required");
-            LOGGER.error("Upload currently not implemented");
-            callback.onSyncFinished();
-        }
-    }
-
-    private boolean isDownloadRequired(final File dbFile, final Metadata metadata) {
-        if (!dbFile.exists()) {
-            return true;
-        }
-
-        long dbFileLastModified = dbFile.lastModified();
-        long onlineLastModified = metadata.getModifiedDate().getTime();
-
-        LOGGER.debug("Current local time:       " + new Date(System.currentTimeMillis()));
-        LOGGER.debug("Last local  modification: " + new Date(dbFileLastModified));
-        LOGGER.debug("Last online modification: " + new Date(onlineLastModified));
-
-        return onlineLastModified > dbFileLastModified;
-    }
-
-    private boolean isUploadRequired(final File dbFile, final Metadata metadata) {
-        if (!dbFile.exists()) {
-            return false;
-        }
-
-        long dbFileLastModified = dbFile.lastModified();
-        long onlineLastModified = metadata.getModifiedDate().getTime();
-
-        return onlineLastModified < dbFileLastModified;
-    }
-
-    private void download(final File dbFile, final Metadata metadata) {
-        LOGGER.debug("Download online database file now...");
-
-        DriveFile file = Drive.DriveApi.getFile(googleApiClient, metadata.getDriveId());
-        PendingResult<DriveApi.ContentsResult> contentsResultPendingResult = file.openContents(googleApiClient,
-                DriveFile.MODE_READ_ONLY, new DriveFile.DownloadProgressListener() {
-                    @Override
-                    public void onProgress(final long downloaded, final long expected) {
-                        LOGGER.debug(String.format("Downloaded = %s  | Expected = %s", downloaded, expected));
-                    }
-                }); // add listener here
-
-        contentsResultPendingResult.setResultCallback(new ResultCallback<DriveApi.ContentsResult>() {
-                @Override
-                public void onResult(final DriveApi.ContentsResult contentsResult) {
-                    LOGGER.debug("Copy online database file to disk");
-                    try {
-                        InputStream in = contentsResult.getContents().getInputStream();
-                        OutputStream out = new FileOutputStream(dbFile);
-
-                        org.apache.commons.io.IOUtils.copy(in, out);
-
-                        LOGGER.info(String.format("Download finished: %s bytes", dbFile.length()));
-
-                        callback.onSyncFinished();
-
-                    } catch (IOException e) {
-                        LOGGER.error("Unable to download database file", e);
-                    }
-                }
-            });
     }
 }
