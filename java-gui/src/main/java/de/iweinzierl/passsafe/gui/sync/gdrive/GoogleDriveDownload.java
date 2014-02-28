ec1b8273ec685eaa63d446c17052b40a292408d8
Ingo Weinzierl <ingo.weinzierl@zalando.de>
2014-02-28 20:42:37 +0100

    Removed the comparison of the database file timestamps which forced an upload.

--- java-gui/src/main/java/de/iweinzierl/passsafe/gui/sync/gdrive/GoogleDriveDownload.java
+++ java-gui/src/main/java/de/iweinzierl/passsafe/gui/sync/gdrive/GoogleDriveDownload.java
@@ -11,7 +11,6 @@ import org.slf4j.LoggerFactory;
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpResponse;
-import com.google.api.client.util.DateTime;
 import com.google.api.client.util.IOUtils;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
@@ -19,41 +18,25 @@ import com.google.api.services.drive.model.FileList;
 
 import com.google.common.base.Strings;
 
-import de.iweinzierl.passsafe.gui.configuration.Configuration;
-
 public class GoogleDriveDownload {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GoogleDriveDownload.class);
 
     private final GoogleDriveSync googleDriveSync;
-    private final Configuration configuration;
     private final Drive client;
 
-    public GoogleDriveDownload(final GoogleDriveSync googleDriveSync, final Configuration configuration,
-            final Drive client) {
-
+    public GoogleDriveDownload(final GoogleDriveSync googleDriveSync, final Drive client) {
         this.googleDriveSync = googleDriveSync;
-        this.configuration = configuration;
         this.client = client;
     }
 
     public void download(final String filename, final java.io.File destination) {
         try {
             File online = find(filename);
-            java.io.File local = getLocalFile(filename);
 
-            DateTime onlineModificationDate = getOnlineModificationDate(online);
-
-            long onlineModificationTime = onlineModificationDate != null ? onlineModificationDate.getValue() : 0;
-            long localModificationDate = getLocalModificationDate(local);
-
-            if (onlineModificationTime > localModificationDate) {
-                long diff = onlineModificationTime - localModificationDate;
-                LOGGER.info("File '{}' needs to be downloaded from GoogleDrive: {} sec younger", filename, diff / 1000);
+            if (online != null) {
                 download(online, destination);
             } else {
-                long diff = localModificationDate - onlineModificationTime;
-                LOGGER.info("File '{}' needs to be uploaded to GoogleDrive: {} sec younger", filename, diff / 1000);
                 googleDriveSync.onStateChanged(GoogleDriveSync.State.UPLOAD_REQUIRED);
             }
         } catch (IOException ioe) {
@@ -81,23 +64,6 @@ public class GoogleDriveDownload {
         } while (!Strings.isNullOrEmpty(request.getPageToken()));
 
         return null;
-    }
-
-    private DateTime getOnlineModificationDate(final File online) throws IOException {
-        if (online == null) {
-            return null;
-        }
-
-        return online.getModifiedDate();
-    }
-
-    private java.io.File getLocalFile(final String filename) {
-        java.io.File passSafeDirectory = new java.io.File(configuration.getBaseFolder());
-        return new java.io.File(passSafeDirectory, filename);
-    }
-
-    private long getLocalModificationDate(final java.io.File file) {
-        return file.exists() ? file.lastModified() : 0;
     }
 
     private void download(final File file, final java.io.File destination) throws IOException {
